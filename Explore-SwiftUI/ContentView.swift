f9ae6a4f58a3d599e93732ac87910df48178e16f
Hanif <hanifsgy@gmail.com>
2019-06-22 21:54:55 +0700

    Initial Commit

---
+++ Explore-SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Explore-SwiftUI
+//
+//  Created by Muhammad Hanif Sugiyanto on 22/06/19.
+//  Copyright © 2019 Muhammad Hanif Sugiyanto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
